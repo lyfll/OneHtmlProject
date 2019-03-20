35e722348daf17adb2568190f1f070b009dc9fba
lyfll <3299515057@qq.com>
2019-03-20 09:07:40 +0800

    改动1

--- src/main/java/com/flank/MyoaApplication.java
+++ src/main/java/com/flank/MyoaApplication.java
@@ -3,7 +3,7 @@ package com.flank;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//myljfsfsf
 @SpringBootApplication
 @MapperScan("com.flank.mapper")
 public class MyoaApplication {
